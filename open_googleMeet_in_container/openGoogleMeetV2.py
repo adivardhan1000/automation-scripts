01be8c677badebb29f5b1e3c3f9e14e66cc3eebb
adivardhan1000 <48666834+adivardhan1000@users.noreply.github.com>
2020-08-07 12:41:00 +0530

    Create openGoogleMeetV2.py

---
+++ open_googleMeet_in_container/openGoogleMeetV2.py
@@ -0,0 +1,19 @@
+import webbrowser as wb
+import pyautogui as pag
+import time
+
+wb.open('https://meet.google.com')
+
+time.sleep(5)
+
+pag.typewrite(['enter'])
+
+time.sleep(3)
+pag.hotkey('ctrl','d')
+time.sleep(1)
+pag.hotkey('ctrl','e')
+time.sleep(1)
+pag.moveTo(x=1343,y=585,duration=1) #join_now
+pag.click()
+print('done')
+
